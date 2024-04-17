bd3916fd0952185e6df4c43e2f35d43dcff7e9ef
Guillaume Garnier <guillaume.garnier@inria.fr>
2024-04-17 13:01:11 +0200

    Add requirements and improvement of Ansatz

--- src/models/ansatz.py
+++ src/models/ansatz.py
@@ -1,5 +1,16 @@
+import torch
+from torch import nn
+import warnings
+import matplotlib.pyplot as plt
+
+import warnings
+import torch
+import torch as tc
+from torch.func import jacfwd, vmap, grad,jacrev
+
+
 class Ansatz(tc.nn.Module):
-    
+
     def __init__(self, d, m, theta=None, kind='gaussian'):
         
         super().__init__()
@@ -48,3 +59,41 @@ class Ansatz(tc.nn.Module):
     
     def __call__(self, x):
         return self.forward(x)
+    
+    
+if __name__ == '__main__':
+    print("> Example of utilisation for the Class Anstatz")
+    
+    # Definition of the space domain and the fonction to approximate
+    x_domain = [0.0, 1.0]; n_points_x = 1000
+    mesh_x = torch.linspace(x_domain[0], x_domain[1], steps=n_points_x, requires_grad=True).unsqueeze(1)
+    u0 = lambda x : (1-x)**2 * x**2
+    y_true = u0(mesh_x).squeeze()
+    
+    # Definition of the model
+    d = 1
+    m = 10
+    model = Ansatz(d,m)
+    
+    # Training
+    criterion=torch.nn.MSELoss()
+    optimizer=torch.optim.Adam(model.parameters(),lr=1e-3)
+
+    total_acc, total_loss=[], []
+    for epoch in range(30000):
+        y_pred=model(mesh_x)
+        loss = criterion(y_pred,y_true)
+        if epoch%3000 == 0:
+            print('epoch:',epoch,'| loss: ', loss)
+        optimizer.zero_grad()
+        loss.backward(retain_graph=True) # back propagation
+        optimizer.step()
+    print(' - Training is complete')
+    
+    # Visualisation
+    y_train = model(mesh_x)
+    fig = plt.figure(figsize=(15, 7))
+    plt.plot(mesh_x.detach(), y_train.detach(), 'k', lw=3)
+    plt.plot(mesh_x.detach(), y_true.detach(),'r:', lw=5)
+    plt.grid()
+    plt.show()
